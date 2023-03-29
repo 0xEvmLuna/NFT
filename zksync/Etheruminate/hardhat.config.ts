d7d2da100f22a2eb07557829b4f82a907719b946
0xEvmLuna <116624440+0xEvmLuna@users.noreply.github.com>
2023-03-28 19:40:47 -0700

    Update hardhat.config.ts

--- zksync/Etheruminate/hardhat.config.ts
+++ zksync/Etheruminate/hardhat.config.ts
@@ -14,14 +14,14 @@ module.exports = {
   networks: {
     zkTestnet: {
       url: "https://zksync2-testnet.zksync.dev", // URL of the zkSync network RPC
-      ethNetwork: "https://eth-goerli.g.alchemy.com/v2/8pjvTLWK2Cr0bCf04Ih4IlkLRq5o82UO", 
+      ethNetwork: "https://eth-goerli.g.alchemy.com/v2/xx", 
       zksync: true,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
       verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
     },
   },
   etherscan: {
-    apiKey: 'RJQZ7PTEDJ6QEDUBA2FII9KG8VUJA78F3P'
+    apiKey: 'xx'
   },
   solidity: {
     version: "0.8.12",
